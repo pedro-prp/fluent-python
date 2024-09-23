171f04f06f104a5825bec02f19a2922a86d6efa1
Pedro Rodrigues <pedro.rodrigues@tradexsolutions.com.br>
2024-09-23 14:29:14 -0300

    Add list comprehension examples

---
+++ 2_colecao_de_sequencias/listcomp.py
@@ -0,0 +1,31 @@
+# Criando lista com códigos dos caracteres da string
+symbols = "$&@*]}=§"
+
+codes = []
+for symbol in symbols:
+    codes.append(ord(symbol))
+
+print("Construção de uma lista a partir de uma string\n")
+
+print("(for): ", codes)
+
+codes = [ord(symbol) for symbol in symbols]
+print("(listcomp): ", codes)
+
+# Comparação entre listcomps e map/filter
+codes_ascii_listcomp = [ord(s) for s in symbols if ord(s) > 127]
+
+codes_ascii_map = list(filter(lambda c: c > 127, map(ord, symbols)))
+
+print("\n(ord) > 127")
+print("(listcomp): ", codes_ascii_listcomp)
+print("(map/filter): ", codes_ascii_map)
+
+print("\nProduto cartesiano usando uma list comprehension\n")
+
+colors = ["black", "white"]
+sizes = ["S", "M", "L"]
+
+tshirts = [(color, size) for color in colors for size in sizes]
+
+print("(C X S): ", tshirts)
